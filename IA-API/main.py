9a74d92e1c3f58458e4b88a843d299c323534c36
portatilJJS <jjsanchezc@eafit.edu.co>
2023-10-28 06:06:42 -0500

    añadi el folder del dockerfile y docker-compose para la api

---
+++ IA-API/main.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+import firebase_admin
+from firebase_admin import credentials, db
+
+app = Flask(__name__)
+
+# Inicializar la app de Firebase
+cred = credentials.Certificate('path/to/serviceAccountKey.json')  # Reemplaza con la ruta de tu archivo JSON de credenciales
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://tu_app.firebaseio.com'  # Reemplaza con la URL de tu base de datos Firebase
+})
+
+# Ruta para obtener valores de Firebase sin parámetros
+@app.route('/get_data', methods=['GET'])
+def get_data():
+    ref = db.reference('/data')  # Reemplaza con la ruta de tu base de datos en Firebase
+    data = ref.get()
+    return jsonify(data)
+
+# Ruta para obtener valores de Firebase con un parámetro entero
+@app.route('/get_data_by_id/<int:item_id>', methods=['GET'])
+def get_data_by_id(item_id):
+    ref = db.reference('/data')  # Reemplaza con la ruta de tu base de datos en Firebase
+    item = ref.child(str(item_id)).get()
+    return jsonify(item)
+
+if __name__ == '__main__':
+    app.run(debug=True)
